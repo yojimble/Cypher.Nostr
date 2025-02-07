770854f8330f069a1110873ba26084005c8a46f2
Gemma <168846021+yojimble@users.noreply.github.com>
2025-02-07 13:26:12 +0000

    Update Ticker.vue

--- components/Funds/Ticker.vue
+++ components/Funds/Ticker.vue
@@ -5,14 +5,14 @@ const btcprice = await $fetch(
   "https://api.coinbase.com/v2/exchange-rates?currency=BTC"
 );
 
-const btcprices = Number(btcprice.data.rates.USD).toFixed(2);
+const btcprices = Number(btcprice.data.rates.GBP).toFixed(2);
 </script>
 <template>
   <div>
     <p
       class="text-xs leading-5 text-black dark:text-black dark:text-white mt-12 block text-center"
     >
-      Bitcoin Ticker: {{ btcprices }} $
+      Bitcoin Ticker: {{ btcprices }} £
     </p>
   </div>
 </template>

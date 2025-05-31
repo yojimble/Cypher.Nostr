b971404a0ee100bc1fd0be736ff6929f883a03ff
Gemma <168846021+yojimble@users.noreply.github.com>
2025-05-31 19:40:08 +0100

    Update Contact.vue
    removed tos

--- components/Contact.vue
+++ components/Contact.vue
@@ -147,36 +147,7 @@
             />
           </div>
         </div>
-        <SwitchGroup as="div" class="flex gap-x-4 sm:col-span-2">
-          <div class="flex h-6 items-center">
-            <Switch
-              v-model="agreed"
-              :class="[
-                agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
-              ]"
-            >
-              <span class="sr-only">Agree to policies</span>
-              <span
-                aria-hidden="true"
-                :class="[
-                  agreed ? 'translate-x-3.5' : 'translate-x-0',
-                  'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out',
-                ]"
-              />
-            </Switch>
-          </div>
-          <SwitchLabel class="text-sm leading-6 text-black dark:text-white">
-            {{ t("consent") }}
-            {{ " " }}
-
-            <NuxtLink
-              :to="localePath('/tos')"
-              class="font-semibold text-black dark:text-white underline"
-              >{{ t("termsOfService") }}</NuxtLink
-            >.
-          </SwitchLabel>
-        </SwitchGroup>
+     
       </div>
       <div class="mt-10">
         <button
